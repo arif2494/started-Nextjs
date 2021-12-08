da0996620829ea467ea68e80efb1fb682ee19b1e
Ariful Islam <arifulislam161024@gmail.com>
2021-12-08 23:26:13 +0600

    added redirect to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
-
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  });
   return (
     <div>
       <div className='not-found'>
