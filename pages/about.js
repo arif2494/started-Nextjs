cf80396c546aaba7e09070b2dcc8d1bb7fc77a70
Ariful Islam <arifulislam161024@gmail.com>
2021-12-10 02:01:07 +0600

    initialkize

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,33 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
-        accusamus tempora. Dolore eligendi, blanditiis recusandae perspiciatis
-        quisquam architecto consequuntur dolores labore vitae facilis quibusdam
-        libero fugiat nulla corporis numquam repellat. Magni accusamus
-        necessitatibus, aperiam eius quaerat dolorum. Odit similique facilis at
-        ut nisi! Aut officia velit facilis totam alias exercitationem.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquid
-        nostrum doloremque dolorem maiores ipsam, a dolore alias commodi eos
-        minus, provident quae placeat, quas iste quam fugit repellat suscipit
-        perferendis. Ipsum vel saepe nisi obcaecati recusandae repellat,
-        voluptatibus, quaerat deleniti eligendi quas exercitationem
-        reprehenderit molestias, tempore labore illo et!
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='ninjas'></meta>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+          accusamus tempora. Dolore eligendi, blanditiis recusandae perspiciatis
+          quisquam architecto consequuntur dolores labore vitae facilis
+          quibusdam libero fugiat nulla corporis numquam repellat. Magni
+          accusamus necessitatibus, aperiam eius quaerat dolorum. Odit similique
+          facilis at ut nisi! Aut officia velit facilis totam alias
+          exercitationem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          aliquid nostrum doloremque dolorem maiores ipsam, a dolore alias
+          commodi eos minus, provident quae placeat, quas iste quam fugit
+          repellat suscipit perferendis. Ipsum vel saepe nisi obcaecati
+          recusandae repellat, voluptatibus, quaerat deleniti eligendi quas
+          exercitationem reprehenderit molestias, tempore labore illo et!
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,37 @@
+import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Home Page</h1>
-      <p className={styles.text}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
-        accusamus tempora. Dolore eligendi, blanditiis recusandae perspiciatis
-        quisquam architecto consequuntur dolores labore vitae facilis quibusdam
-        libero fugiat nulla corporis numquam repellat. Magni accusamus
-        necessitatibus, aperiam eius quaerat dolorum. Odit similique facilis at
-        ut nisi! Aut officia velit facilis totam alias exercitationem.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquid
-        nostrum doloremque dolorem maiores ipsam, a dolore alias commodi eos
-        minus, provident quae placeat, quas iste quam fugit repellat suscipit
-        perferendis. Ipsum vel saepe nisi obcaecati recusandae repellat,
-        voluptatibus, quaerat deleniti eligendi quas exercitationem
-        reprehenderit molestias, tempore labore illo et!
-      </p>
-      <Link href='/ninjas' className={styles.btn}>
-        <a>Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name='keywords' content='ninjas'></meta>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home Page</h1>
+        <p className={styles.text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+          accusamus tempora. Dolore eligendi, blanditiis recusandae perspiciatis
+          quisquam architecto consequuntur dolores labore vitae facilis
+          quibusdam libero fugiat nulla corporis numquam repellat. Magni
+          accusamus necessitatibus, aperiam eius quaerat dolorum. Odit similique
+          facilis at ut nisi! Aut officia velit facilis totam alias
+          exercitationem.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          aliquid nostrum doloremque dolorem maiores ipsam, a dolore alias
+          commodi eos minus, provident quae placeat, quas iste quam fugit
+          repellat suscipit perferendis. Ipsum vel saepe nisi obcaecati
+          recusandae repellat, voluptatibus, quaerat deleniti eligendi quas
+          exercitationem reprehenderit molestias, tempore labore illo et!
+        </p>
+        <Link href='/ninjas' className={styles.btn}>
+          <a>Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
