35b51c114bdebb265a5a89487bfdfff9b9fe81af
Ariful Islam <arifulislam161024@gmail.com>
2021-12-09 07:24:49 +0600

    fetch from api added

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,23 @@
-const index = () => {
+import styles from "../../styles/Ninjas.module.css";
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+const index = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a>
+            <h3 className={styles.single}>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
