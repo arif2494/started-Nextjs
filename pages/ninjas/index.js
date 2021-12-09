cea89bd8292edf017f05645fa5fcb9ed1ba9595b
Ariful Islam <arifulislam161024@gmail.com>
2021-12-09 11:53:43 +0600

    dynamic route added

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "../../styles/Ninjas.module.css";
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -12,11 +13,11 @@ const index = ({ ninjas }) => {
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map((ninja) => (
-        <div key={ninja.id}>
+        <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
           <a>
             <h3 className={styles.single}>{ninja.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
